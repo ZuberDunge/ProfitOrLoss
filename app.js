a133ebec05e7f3b4aa3ad21abb177360c97b30f2
ZuberDunge <iamZuberDunge@gmail.com>
2021-09-13 11:33:01 +0530

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const stocksQuantity = document.querySelector('#stocks-quantity');
 const currentPrice = document.querySelector('#current-price');
 const checkButton = document.querySelector('#check-button');
 const outputBox = document.querySelector('#output-box');
+const errorBox = document.querySelector('#error-box');
 var loadingImg  = document.querySelector('.loading-img');
 
 
@@ -14,60 +15,58 @@ function checkHandeler(){
     var inPrice = Number(initialPrice.value);
     var stQuantity = Number(stocksQuantity.value);
     var curPrice = Number(currentPrice.value);
+   
+    if(inPrice>0&&stQuantity>0&&curPrice>0)
     calculateProfitAndLoss(inPrice, stQuantity, curPrice);
-
-    if(inPrice&&stQuantity&&curPrice)
-    compareVlues(inPrice, luckyNumber.value, curPrice.value);
     else
-    outputBox.style.display = `block`;
-    outputBox.innerText="Please Enter all fields!"
+   errorBox.style.display = `block`;
+   errorBox.innerText="Please Enter all fields!"
 
+    
 
 }
 
 function calculateProfitAndLoss(initial, quantity, current) {
 
+
+
+
     if (initial > current) {
-     
-
-setTimeout(function(){  
-    var loss = (initial - current)*quantity;
-    var lossPercentage = (loss / (initial * quantity)) * 100;
-
-
-
-
-        loadingImg.style.display = `none`;       
-        outputBox.style.display = `block`;    
-        outputBox.innerText=`the loss is ${loss} and the percent is ${lossPercentage}%`;
-        }, 3000);
-
-        loadingImg.style.display = `block`;      
-        outputBox.innerText = 'Hang on our robot is working on it!';
-        outputBox.style.display = `block`;
-
-
-
-        
-    } else if (current > initial) {
-        setTimeout(function(){ 
-
-            var profit = (current-initial)*quantity;
-            var profitPercentage = (profit / (initial * quantity)) * 100;
-            
+        setTimeout(function(){  
+            var loss = (initial - current)*quantity;
+            var lossPercentage = (loss / (initial * quantity)) * 100;
             loadingImg.style.display = `none`;       
             outputBox.style.display = `block`;    
-            outputBox.innerText=`the profit is ${profit} and the percent is ${profitPercentage}%`;
-
+            outputBox.innerText=`the loss is ${loss} and the percent is ${lossPercentage}%`;
             }, 3000);
 
-            loadingImg.style.display = `block`;      
-            outputBox.innerText = 'Hang on our robot is working on it!';
-            outputBox.style.display = `block`;
-     } else {
-              
-            outputBox.innerText="No Gain! No Pain!";
-            
-    }
+                errorBox.style.display = `none`;
+                loadingImg.style.display = `block`;      
+                outputBox.innerText = 'Hang on our robot is working on it!';
+                outputBox.style.display = `block`;
+
+
+
+                
+            } else if (current > initial) {
+                setTimeout(function(){ 
+                    errorBox.style.display = `none`;
+                    var profit = (current-initial)*quantity;
+                    var profitPercentage = (profit / (initial * quantity)) * 100;
+                    
+                    loadingImg.style.display = `none`;       
+                    outputBox.style.display = `block`;    
+                    outputBox.innerText=`the profit is ${profit} and the percent is ${profitPercentage}%`;
+
+                    }, 3000);
+                    errorBox.style.display = `none`;
+                    loadingImg.style.display = `block`;      
+                    outputBox.innerText = 'Hang on our robot is working on it!';
+                    outputBox.style.display = `block`;
+            } else {
+                errorBox.style.display = `none`;
+                    outputBox.innerText="No Gain! No Pain!";
+                    
+            }
 }
 
